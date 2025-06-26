2ea391ddbf71b5805c667942bc144eb744ab944f
Sabbir Hosain <sabbirhosainbd59@gamil.com>
2025-06-27 00:18:00 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.js"
 import { Route, Routes } from "react-router-dom"
-import './App.css'
 import Home from "./Pages/Home/Home"
 import PremiumTools from "./Pages/PremiumTools/PremiumTools"
 import CheckOut from "./Pages/CheckOut/CheckOut"
 import ToolsDetails from "./Pages/ToolsDetails/ToolsDetails"
+import OrderConfirm from "./Pages/OrderConfirm/OrderConfirm"
+import './App.css'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/premium-tools" element={<PremiumTools />} />
         <Route path="/premium-tools/tools-details" element={<ToolsDetails />} />
         <Route path="/premium-tools/check-out" element={<CheckOut />} />
+        <Route path="/premium-tools/order-confirm" element={<OrderConfirm />} />
       </Routes>
     </>
   )
---
+++ src/Pages/OrderConfirm/OrderConfirm.jsx
@@ -0,0 +1,29 @@
+import Layout from '../../Layout/Layout'
+import { FaCheck } from "react-icons/fa6";
+import './OrderConfirm.css'
+
+const OrderConfirm = () => {
+  return (
+    <Layout>
+      <section className="order_confirm_section">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-8">
+              <div className='order_confirm_text'>
+                <div className='order_confirm_icon'><FaCheck /></div>
+                <h5>Order Confirmed!</h5>
+                <h4>Your order has been placed successfully.</h4>
+                <p>A confirmation email with the details of your order has been sent to</p>
+                <strong>your.email@example.com</strong>
+                <p className='pt-5 text-danger'>আমরা কিছুক্ষণের মধ্যে আপনার সাথে যোগাযোগ করব। অনুগ্রহ করে অপেক্ষা করুন।</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </Layout >
+  )
+}
+
+export default OrderConfirm
