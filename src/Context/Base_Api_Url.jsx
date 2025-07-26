68de1baf2f7c0c4e47954a976bf2ab2040668dba
Sabbir Hosain <sabbirhosainbd59@gmail.com>
2025-07-26 10:19:05 +0500

    update

--- src/Context/Base_Api_Url.jsx
+++ src/Context/Base_Api_Url.jsx
@@ -5,7 +5,7 @@ export const itemShow = import.meta.env.VITE_API_BASE_URL + "/api/v1/items/premi
 export const itemDetails = import.meta.env.VITE_API_BASE_URL + "/api/v1/items/premium/product/";
 export const freeItemShow = import.meta.env.VITE_API_BASE_URL + "/api/v1/items/free/product";
 export const freeItemDetails = import.meta.env.VITE_API_BASE_URL + "/api/v1/items/free/product/";
-export const checkOut = import.meta.env.VITE_API_BASE_URL + "/api/v1/items/product/checkout";
-export const freeCheckOut = import.meta.env.VITE_API_BASE_URL + "/api/v1/items/free/product/checkout";
+export const checkOut = import.meta.env.VITE_API_BASE_URL + "/api/v1/items/premium/checkout";
+export const freeCheckOut = import.meta.env.VITE_API_BASE_URL + "/api/v1/items/free/checkout";
 
 
